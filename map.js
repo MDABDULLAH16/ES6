a845fc9716540fe1312e9ffc42433ae15904296a
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-08-31 18:40:35 +0600

    map,filter and find method

---
+++ map.js
@@ -0,0 +1,20 @@
+const numbers = [2, 4, 56, 7, 5, 23];
+// doubled with loop
+const doubledWithLoop = [];
+for (const num of numbers) {
+  const result = num * 2;
+  doubledWithLoop.push(result);
+}
+console.log({ doubledWithLoop });
+
+// \\doubled with map
+const doubled = numbers.map((num) => num * 2);
+console.log({ doubled });
+
+// return array if the condition match filter
+const filtered = numbers.filter((num) => num % 2);
+
+console.log({ filtered });
+// return the first Element after match the condition
+const find = numbers.find((num) => num % 2);
+console.log({ find });
