5350640a431271a8e6dd11f6e7817b4670c945ef
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-09-04 22:01:47 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,11 @@
+// doMath(2);
+const doMath = (x) => {
+  const result = x * 2;
+  console.log(result);
+};
+doMath(2);
+doMath2(4);
+function doMath2(x) {
+  const result = x * 3;
+  console.log(result);
+}
